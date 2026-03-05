aee9058811747421be6a82fed083b64855112f2c
Yaroslav-Chertov <yachertov@yandex.ru>
2026-03-05 14:39:09 +0300

    fix fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", "ui-sans-serif", "system-ui"],
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
       },
     },
   },
